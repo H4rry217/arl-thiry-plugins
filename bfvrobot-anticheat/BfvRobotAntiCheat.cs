8988c1c92712ba619cb1c44a1a5131c54e0250fd
H4rry217 <hcilnete@gmail.com>
2023-10-08 17:44:29 +0800

    bfvrobot社区黑名单屏蔽插件

---
+++ bfvrobot-anticheat/BfvRobotAntiCheat.cs
@@ -0,0 +1,100 @@
+﻿using AimRobot.Api;
+using AimRobot.Api.game;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace bfvrobot_anticheat {
+    public class BfvRobotAntiCheat : IAntiCheat {
+
+        public delegate void DataCallback(object param);
+
+        public void IsAbnormalPlayer(string name, IAntiCheat.CheckResult checkResult) {
+            Robot.GetInstance().GetGameContext().GetPlayerStatInfo(name, (statData) => {
+                IsAbnormalPlayer(statData.id, checkResult);
+            });
+        }
+
+        public void IsAbnormalPlayer(long playerId, IAntiCheat.CheckResult checkResult) {
+            var data = BfvRobotPlugin.config.GetData($"{playerId}");
+
+            try {
+                if (data != null) {
+                    var vals = data.Split(",");
+
+                    long curTime = DateTimeOffset.Now.ToUnixTimeSeconds();
+                    long dataCreateTime = long.Parse(vals[1]);
+
+                    if (((curTime - dataCreateTime) / (float)86400) < 3) {
+                        int status = int.Parse(vals[0]);
+                        if (GameConst.BfvRobotCommunityAbnormalStatus.ContainsKey(int.Parse(vals[0]))) {
+                            checkResult(true, GameConst.BfvRobotCommunityAbnormalStatus[status], GameConst.BfvRobotCommunityAbnormalStatus[status]);
+                        } else {
+                            checkResult(false, null, null);
+                        }
+                        return;
+                    }
+                }
+            }catch(Exception ex) {
+                BfvRobotPlugin.plugin.GetLogger().Error($"从本地获取bfv robot数据发生异常！{ex}");
+            }
+
+            GetBfvRobotStat(playerId, (data) => {
+                var json = ((JsonDocument)data).RootElement;
+                string message = json.GetProperty("message").GetString();
+
+                if (json.GetProperty("status").GetInt32() == 1) {
+                    if (string.Equals(message, "successful")) {
+                        try {
+                            var status = (json.GetProperty("data")[0].GetString());
+                            int statusInt = int.Parse(status);
+
+                            if (GameConst.BfvRobotCommunityAbnormalStatus.ContainsKey(statusInt)) {
+                                checkResult(true, GameConst.BfvRobotCommunityAbnormalStatus[statusInt], GameConst.BfvRobotCommunityAbnormalStatus[statusInt]);
+                            } else {
+                                checkResult(false, null, null);
+                            }
+
+                            BfvRobotPlugin.config.SetData($"{playerId}", $"{statusInt},{DateTimeOffset.Now.ToUnixTimeSeconds()}");
+
+                        } catch (Exception ex) {
+                            BfvRobotPlugin.plugin.GetLogger().Error($"获取bfv robot数据发生异常！{ex}");
+                        }
+                    } else {
+                        checkResult(false, null, null);
+                    }
+                }
+            });
+        }
+
+        public async void GetBfvRobotStat(long playerId, DataCallback callback) {
+            string data = await Get($"https://api.zth.ink/api/findPlayerStatus?pid={playerId}");
+
+            if (!string.Equals(string.Empty, data)) callback(JsonDocument.Parse(data));
+        }
+
+        public async Task<string> Get(string url) {
+
+            using (HttpClient httpClient = new HttpClient()) {
+
+                try {
+                    HttpResponseMessage response = await httpClient.GetAsync(url);
+                    if (response.IsSuccessStatusCode) {
+                        return await response.Content.ReadAsStringAsync();
+                    } else {
+                        return string.Empty;
+                    }
+                } catch (HttpRequestException ex) {
+                    return string.Empty;
+                }
+            }
+
+        }
+
+    }
+}
---
+++ bfvrobot-anticheat/BfvRobotPlugin.cs
@@ -0,0 +1,47 @@
+﻿using AimRobot.Api;
+using AimRobot.Api.config;
+using AimRobot.Api.plugin;
+
+namespace bfvrobot_anticheat {
+    public class BfvRobotPlugin : PluginBase {
+
+        public static AutoSaveConfig config;
+        public static BfvRobotPlugin plugin;
+
+        private IAntiCheat antiCheat;
+
+        public override string GetAuthor() {
+            return "H4rry217";
+        }
+
+        public override string GetDescription() {
+            return "屏蔽BFV ROBOT社区全局黑名单玩家的插件";
+        }
+
+        public override string GetPluginName() {
+            return "bfvrobot_anticheat";
+        }
+
+        public override Version GetVersion() {
+            return new Version(1, 0, 0);
+        }
+
+        public override void OnDisable() {
+            Robot.GetInstance().GetGameContext().UnregisterAntiCheat(this.antiCheat);
+        }
+
+        public override void OnEnable() {
+            Robot.GetInstance().GetGameContext().RegisterAntiCheat(this.antiCheat);
+        }
+
+        public override void OnLoad() {
+            plugin = this;
+            config = Robot.GetInstance().GetPluginManager().GetDefaultAutoSaveConfig(this, "bfvrobot");
+            Robot.GetInstance().GetPluginManager().ConfigAutoSave(config);
+
+            this.antiCheat = new BfvRobotAntiCheat();
+
+        }
+
+    }
+}
